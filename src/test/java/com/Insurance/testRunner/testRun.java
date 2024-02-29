958e3fd5b021260503eec11c83704f376c23b435
Deepanshu <Deepanshuverma938@gmail.com>
2024-02-29 16:10:54 +0530

    testrunner update

--- src/test/java/com/Insurance/testRunner/testRun.java
+++ src/test/java/com/Insurance/testRunner/testRun.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = { ".//Features/InsuranceSmoke.feature" }, glue = "com.Insurance.stepDefinitions", plugin = {
-		"pretty", "html:reports/myreport.html", "rerun:target/rerun.txt",
+@CucumberOptions(features = { ".//Features/" }, glue = "com.Insurance.stepDefinitions", plugin = { "pretty",
+		"html:reports/myreport.html", "rerun:target/rerun.txt",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, dryRun = false, monochrome = true)
 
 public class testRun extends AbstractTestNGCucumberTests {
